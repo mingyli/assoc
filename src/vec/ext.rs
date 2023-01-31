96b09329763809ebce3e7099fca33f79fb903e68
ming li <mingyli34@gmail.com>
2023-01-30 22:09:26 -0500

    Implement methods for getting iterators of keys and values (#3)
    * Implement methods for getting iterators of keys and values
    
    * Add methods to strict trait
    
    * Add unused warning
    
    * Remove KeysMut
    
    * Implement Debug
    
    * Remove mut from doctests

--- src/vec/ext.rs
+++ src/vec/ext.rs
@@ -1,6 +1,156 @@
 use std::borrow::Borrow;
+use std::fmt;
+use std::slice::{Iter, IterMut};
+use std::vec::IntoIter;
 
 use crate::vec::{Entry, OccupiedEntry, VacantEntry};
+
+#[must_use = "iterators are lazy and do nothing unless consumed"]
+pub struct Keys<'a, K: 'a, V: 'a> {
+    inner: Iter<'a, (K, V)>,
+}
+
+impl<'a, K, V> Iterator for Keys<'a, K, V> {
+    type Item = &'a K;
+
+    fn next(&mut self) -> Option<&'a K> {
+        self.inner.next().map(|(k, _)| k)
+    }
+}
+
+impl<K: fmt::Debug, V> fmt::Debug for Keys<'_, K, V> {
+    /// ```rust
+    /// use assoc::AssocExt;
+    ///
+    /// let map = vec![("a", 1), ("b", 2)];
+    /// assert_eq!(format!("{:?}", map.keys()), r#"["a", "b"]"#);
+    /// ```
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list().entries(self.clone()).finish()
+    }
+}
+
+impl<K, V> Clone for Keys<'_, K, V> {
+    fn clone(&self) -> Self {
+        Keys {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
+#[must_use = "iterators are lazy and do nothing unless consumed"]
+pub struct IntoKeys<K, V> {
+    inner: IntoIter<(K, V)>,
+}
+
+impl<K, V> Iterator for IntoKeys<K, V> {
+    type Item = K;
+
+    fn next(&mut self) -> Option<K> {
+        self.inner.next().map(|(k, _)| k)
+    }
+}
+
+impl<K: fmt::Debug, V> fmt::Debug for IntoKeys<K, V> {
+    /// ```rust
+    /// use assoc::AssocExt;
+    ///
+    /// let map = vec![("a", 1), ("b", 2)];
+    /// assert_eq!(format!("{:?}", map.into_keys()), r#"["a", "b"]"#);
+    /// ```
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list()
+            .entries(self.inner.as_slice().iter().map(|(k, _)| k))
+            .finish()
+    }
+}
+
+#[must_use = "iterators are lazy and do nothing unless consumed"]
+pub struct Values<'a, K: 'a, V: 'a> {
+    inner: Iter<'a, (K, V)>,
+}
+
+impl<'a, K, V> Iterator for Values<'a, K, V> {
+    type Item = &'a V;
+
+    fn next(&mut self) -> Option<&'a V> {
+        self.inner.next().map(|(_, v)| v)
+    }
+}
+
+impl<K, V: fmt::Debug> fmt::Debug for Values<'_, K, V> {
+    /// ```rust
+    /// use assoc::AssocExt;
+    ///
+    /// let map = vec![("a", 1), ("b", 2)];
+    /// assert_eq!(format!("{:?}", map.values()), "[1, 2]");
+    /// ```
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list().entries(self.clone()).finish()
+    }
+}
+
+impl<K, V> Clone for Values<'_, K, V> {
+    fn clone(&self) -> Self {
+        Values {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
+#[must_use = "iterators are lazy and do nothing unless consumed"]
+pub struct ValuesMut<'a, K: 'a, V: 'a> {
+    inner: IterMut<'a, (K, V)>,
+}
+
+impl<'a, K, V> Iterator for ValuesMut<'a, K, V> {
+    type Item = &'a mut V;
+
+    fn next(&mut self) -> Option<&'a mut V> {
+        self.inner.next().map(|(_, v)| v)
+    }
+}
+
+impl<K, V: fmt::Debug> fmt::Debug for ValuesMut<'_, K, V> {
+    /// ```rust
+    /// use assoc::AssocExt;
+    ///
+    /// let mut map = vec![("a", 1), ("b", 2)];
+    /// assert_eq!(format!("{:?}", map.values_mut()), "[1, 2]");
+    /// ```
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list()
+            .entries(self.inner.as_slice().iter().map(|(_, v)| v))
+            .finish()
+    }
+}
+
+#[must_use = "iterators are lazy and do nothing unless consumed"]
+pub struct IntoValues<K, V> {
+    inner: IntoIter<(K, V)>,
+}
+
+impl<K, V> Iterator for IntoValues<K, V> {
+    type Item = V;
+
+    fn next(&mut self) -> Option<V> {
+        self.inner.next().map(|(_, v)| v)
+    }
+}
+
+impl<K, V: fmt::Debug> fmt::Debug for IntoValues<K, V> {
+    /// ```rust
+    /// use assoc::AssocExt;
+    ///
+    /// let map = vec![("a", 1), ("b", 2)];
+    /// assert_eq!(format!("{:?}", map.into_values()), "[1, 2]");
+    /// ```
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list()
+            .entries(self.inner.as_slice().iter().map(|(_, v)| v))
+            .finish()
+    }
+}
 
 /// A trait extension that allows vectors to be treated as associative arrays.
 pub trait AssocExt<K, V> {
@@ -70,6 +220,72 @@ pub trait AssocExt<K, V> {
     where
         K: Borrow<Q>,
         Q: PartialEq + ?Sized;
+
+    /// Get an iterator over the keys of the map.
+    ///
+    /// ```rust
+    /// use assoc::AssocExt;
+    ///
+    /// let map = vec![("a", 1), ("b", 2)];
+    /// let mut iter = map.keys();
+    ///
+    /// assert_eq!(iter.next().unwrap(), &"a");
+    /// assert_eq!(iter.next().unwrap(), &"b");
+    /// assert_eq!(iter.next(), None);
+    /// ```
+    fn keys(&self) -> Keys<'_, K, V>;
+
+    /// Create a consuming iterator visiting all the keys of the map.
+    ///
+    /// ```rust
+    /// use assoc::AssocExt;
+    ///
+    /// let map = vec![(1, "a"), (2, "b")];
+    /// let keys: Vec<i32> = map.into_keys().collect();
+    /// assert_eq!(keys, [1, 2]);
+    /// ```
+    fn into_keys(self) -> IntoKeys<K, V>;
+
+    /// Get an iterator over the values of the map.
+    ///
+    /// ```rust
+    /// use assoc::AssocExt;
+    ///
+    /// let map = vec![("a", 1), ("b", 2)];
+    /// let mut iter = map.values();
+    ///
+    /// assert_eq!(iter.next().unwrap(), &1);
+    /// assert_eq!(iter.next().unwrap(), &2);
+    /// assert_eq!(iter.next(), None);
+    /// ```
+    fn values(&self) -> Values<'_, K, V>;
+
+    /// Get a mutable iterator over the values of the map.
+    ///
+    /// ```rust
+    /// use assoc::AssocExt;
+    ///
+    /// let mut map = vec![(1, "a".to_string()), (2, "b".to_string())];
+    ///
+    /// for value in map.values_mut() {
+    ///     value.push_str("!");
+    /// }
+    ///
+    /// let values: Vec<String> = map.values().cloned().collect();
+    /// assert_eq!(values, ["a!".to_string(), "b!".to_string()]);
+    /// ```
+    fn values_mut(&mut self) -> ValuesMut<'_, K, V>;
+
+    /// Create a consuming iterator visiting all the values of the map.
+    ///
+    /// ```rust
+    /// use assoc::AssocExt;
+    ///
+    /// let map = vec![(1, "a"), (2, "b")];
+    /// let values: Vec<&str> = map.into_values().collect();
+    /// assert_eq!(values, ["a", "b"]);
+    /// ```
+    fn into_values(self) -> IntoValues<K, V>;
 }
 
 impl<K, V> AssocExt<K, V> for Vec<(K, V)>
@@ -127,6 +343,32 @@ where
                 let (_, v) = self.swap_remove(index);
                 Some(v)
             }
+        }
+    }
+
+    fn keys(&self) -> Keys<'_, K, V> {
+        Keys { inner: self.iter() }
+    }
+
+    fn into_keys(self) -> IntoKeys<K, V> {
+        IntoKeys {
+            inner: self.into_iter(),
+        }
+    }
+
+    fn values(&self) -> Values<'_, K, V> {
+        Values { inner: self.iter() }
+    }
+
+    fn values_mut(&mut self) -> ValuesMut<'_, K, V> {
+        ValuesMut {
+            inner: self.iter_mut(),
+        }
+    }
+
+    fn into_values(self) -> IntoValues<K, V> {
+        IntoValues {
+            inner: self.into_iter(),
         }
     }
 }
@@ -206,6 +448,12 @@ pub trait AssocStrictExt<K, V> {
     where
         K: Borrow<Q>,
         Q: PartialEq + ?Sized;
+
+    fn keys(&self) -> Keys<'_, K, V>;
+    fn into_keys(self) -> IntoKeys<K, V>;
+    fn values(&self) -> Values<'_, K, V>;
+    fn values_mut(&mut self) -> ValuesMut<'_, K, V>;
+    fn into_values(self) -> IntoValues<K, V>;
 }
 
 impl<K, V> AssocStrictExt<K, V> for Vec<(K, V)>
@@ -242,5 +490,25 @@ where
         Q: PartialEq + ?Sized,
     {
         AssocExt::remove(self, key)
+    }
+
+    fn keys(&self) -> Keys<'_, K, V> {
+        AssocExt::keys(self)
+    }
+
+    fn into_keys(self) -> IntoKeys<K, V> {
+        AssocExt::into_keys(self)
+    }
+
+    fn values(&self) -> Values<'_, K, V> {
+        AssocExt::values(self)
+    }
+
+    fn values_mut(&mut self) -> ValuesMut<'_, K, V> {
+        AssocExt::values_mut(self)
+    }
+
+    fn into_values(self) -> IntoValues<K, V> {
+        AssocExt::into_values(self)
     }
 }
